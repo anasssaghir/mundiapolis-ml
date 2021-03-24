360fc678375fa8376413ffb3be23e58dec69f94d
Anass SAGHIR <71654887+anasssaghir@users.noreply.github.com>
2021-03-24 16:33:55 +0100

    Add files via upload

---
+++ 0x01-classification/0-neuron.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+''' la classe des neurones '''
+
+import numpy as np
+
+
+class Neuron:
+    ''' La classe qui a initialisé le neurone '''
+
+    def __init__(self, nx):
+        ''' La fonction de la classe Neuron '''
+
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.W = np.random.normal(0, 1, (1, nx))
+        self.b = 0
+        self.A = 0
