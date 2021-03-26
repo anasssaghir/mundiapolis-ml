af662e182dfdc68bdfd968aef1376832ba4d71a2
Anass SAGHIR <71654887+anasssaghir@users.noreply.github.com>
2021-03-26 14:36:51 +0100

    Update 2-neuron.py

--- 0x01-classification/2-neuron.py
+++ 0x01-classification/2-neuron.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-'''Module contenant la classe neurone'''
+'''Module contenant la classe neurone
+'''
 
 import numpy as np
 
@@ -22,35 +23,31 @@ class Neuron:
         self.__A = 0
 
     def forward_prop(self, X):
-        """
-        calcule de la propagation avant du neurone
-        la variable X: un tableau np avec la forme (nx, m) qui contient les données d'entrées
-        elle retourne: attribut privé __A
-        """
+        '''calcule de la propagation avant du neurone
+            la variable X: un tableau np avec la forme (nx, m) qui contient les données d'entrées
+            elle retourne: attribut privé __A
+        '''
         preactivation = np.matmul(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-preactivation))
         return self.__A
 
     @property
     def W(self):
-        """
-        fonction getter pour W
-        elle retourne: vecteur de poids du neurone
-        """
+        '''fonction getter pour W
+            elle retourne: vecteur de poids du neurone
+        '''
         return self.__W
 
     @property
     def b(self):
-        """
-        fonction getter pour b
-        elle retourne: biais pour le neurone
-        """
+        '''fonction getter pour b
+            elle retourne: biais pour le neurone
+        '''
         return self.__b
 
     @property
     def A(self):
-        """
-        fonction getter pour A
-        elle retourne: sortie activée du neurone
-        """
+        '''fonction getter pour A
+            elle retourne: sortie activée du neurone
+        '''
         return self.__A
