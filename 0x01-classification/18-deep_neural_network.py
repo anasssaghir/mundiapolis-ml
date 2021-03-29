855cd39ecc1810246d629daf27a1cb7edc413a58
Anass SAGHIR <71654887+anasssaghir@users.noreply.github.com>
2021-03-29 17:59:45 +0100

    Update 18-deep_neural_network.py

--- 0x01-classification/18-deep_neural_network.py
+++ 0x01-classification/18-deep_neural_network.py
@@ -7,14 +7,12 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """
-    Une classe qui définit un réseau neuronal profond avec une couche cachée effectuant une classification binaire
+    classification binaire
     """
 
     def __init__(self, nx, layers):
         """
         constructeur de classe
-         paramètre nx: est le nombre d'entités d'entrée du neurone
-         paramètre layers: une liste représentant le nombre de nœuds dans chaque couche
         """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -23,11 +21,8 @@ class DeepNeuralNetwork:
         self.nx = nx
         if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        # L : est le nombre de couches dans le réseau neuronal
         self.__L = len(layers)
-        # cache : est un dictionnaire pour contenir toutes les valeurs intermédiaires du réseau
         self.__cache = {}
-        # weights : est un dictionnaire pour tenir tous les poids et biais du réseau
         weights = {}
         for i in range(len(layers)):
             if layers[i] < 1:
