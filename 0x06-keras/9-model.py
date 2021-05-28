f9686dd40777f8390602466edf9358b3b44eb478
Anass SAGHIR <71654887+anasssaghir@users.noreply.github.com>
2021-05-28 15:03:14 +0100

    Delete 9-model.py

--- 0x06-keras/9-model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow.keras as K
-
-def save_model(network, filename):
-    K.models.save_model(model=network,filepath=filename)
-    return None
-
-
-def load_model(filename):
-    return K.models.load_model(filepath=filename)
